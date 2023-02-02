3cfff6052b67da27de521f67fe6252b96ecd969e
anlola88 <amlopezla@cesde.net>
2023-02-02 12:11:26 -0500

    se crean metodos en clase empleados

--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -1,4 +1,30 @@
 package org.example;
 
 public class Empleado {
+    //ATRIBUTOS
+
+    String nombre = "";
+
+    //CONSTRUCTOR
+
+    public Empleado() {
+
+    }
+
+    //METODOS ESPECIAL
+
+    //LA LICUADORA
+
+
+    public String saludar(String nombresito) {
+        String saludo = "Hola " + nombresito;
+        return saludo;
+    }
+    public int sumar(int numero1, int numero2){
+        int resultado= numero1 + numero2;
+        return resultado;
+        // tambien puedo optimizar mi codigo de la siguiente manera
+        //return  numero1 + numero2;
+    }
+}
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,38 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // para usar una clase debo crear un objeto
+        Empleado objeto = new Empleado();
 
-        //EN EL MAIN LLAMO A MIS CLASES
-        //EN EL MAIN CREO LOS OBJETOS DE MIS CLASES
-        //EN EL MAIN CREO UNA INSTANCIA DE MI CLASE
-        //EN EL MAIN LE SACO COPIA A MIS CLASES Y CREO OBJETOS
-        Invitado objetoDeClaseInvitado=new Invitado();
-        Invitado objeto2=new Invitado(333,true,"Juan",10000);
+        //utiliar el objeto para acceder a um atributo
+        objeto.nombre = "";
+
+        //utilizar el objeto para acceder a un método
+        String resultado=objeto.saludar("Angela López");
+        int sumatoria=objeto.sumar(5,8);
 
 
-        //obtener el valor de los atributos
-        System.out.println(objetoDeClaseInvitado.nombre);
-        System.out.println(objeto2.nombre);
 
+        //SALIDAS POR CONSOLA
+        //acceder a mostrar el valor de un atributo
+        System.out.println(objeto.nombre);
+        //acceder a mostrar el retorno de la funcion
+        System.out.println(objeto.saludar("Angela López"));
+        //Salida de retorno de una función, la idea es crear una variable
+        System.out.println(resultado);
+
+        System.out.println(sumatoria);
     }
-}
+//EN EL MAIN LLAMO A MIS CLASES
+//EN EL MAIN CREO LOS OBJETOS DE MIS CLASES
+//EN EL MAIN CREO UNA INSTANCIA DE MI CLASE
+//EN EL MAIN LE SACO COPIA A MIS CLASES Y CREO OBJETOS
+//Invitado objetoDeClaseInvitado=new Invitado();
+//Invitado objeto2=new Invitado(333,true,"Juan",10000);
+
+
+//obtener el valor de los atributos
+//System.out.println(objetoDeClaseInvitado.nombre);
+//System.out.println(objeto2.nombre);
+
+
